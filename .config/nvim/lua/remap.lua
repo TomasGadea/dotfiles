1f4018b18b4da38d41380b60e72c6fb86cea6e15
Tomás Gadea <01tomas.gadea@gmail.com>
2024-10-12 17:04:21 +0200

    tmux nvim navigation

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -12,3 +12,9 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+
+vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<cr>")
+vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<cr>")
+vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<cr>")
+vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<cr>")
