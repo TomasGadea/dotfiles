3be2d1cf5744f11b4d47e1f248f6a34b3b7436a7
Tomás Gadea <01tomas.gadea@gmail.com>
2024-12-07 20:30:32 +0100

    [nvim-tree] Set auto-close

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -10,6 +10,14 @@ return {
             filters = {
                 dotfiles = false,
             },
+            actions = {
+                open_file = {
+                    quit_on_open = true,
+                    window_picker = {
+                        enable = false, -- Ensure the window picker doesn't interfere
+                    },
+                },
+            },
         })
         vim.keymap.set("n", "<C-n>", ":NvimTreeFindFileToggle<CR>")
         vim.cmd[[hi NvimTreeNormal guibg=NONE ctermbg=NONE]]
