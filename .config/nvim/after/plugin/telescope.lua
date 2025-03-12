a0c850f4cc5a868e99544c1f7e007c32dd40666a
TomasGadea <01tomas.gadea@gmail.com>
2025-03-12 14:37:56 +0100

    [nvim] Include hidden files in telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,17 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({
+        hidden = true,
+        no_ignore = true,
+        file_ignore_patterns = {".git/"}
+    })
+end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.live_grep({
+        additional_args = function()
+            return { "--hidden", "--no-ignore", "--glob", "!**/.git/*" }
+        end
+    })
+end, {})
