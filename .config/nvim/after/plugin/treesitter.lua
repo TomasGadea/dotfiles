e8c4d7831667feca5c9ce8b259fbb00c8667da35
TomasGadea <01tomas.gadea@gmail.com>
2025-03-18 12:24:09 +0100

    [nvim] Add dockerfile treesitter

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "python", "sql", "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline"},
+  ensure_installed = { "javascript", "typescript", "python", "sql", "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "dockerfile"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
