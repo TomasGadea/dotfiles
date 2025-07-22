54611dbc76e436b57d9924a4776b6b0d87fccde4
TomasGadea <01tomas.gadea@gmail.com>
2025-07-22 15:22:30 +0200

    [nvim] Solve highlight brightness

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -7,6 +7,10 @@ return {
       require("rose-pine").setup({
         --- optional default variant:
         variant = "rose", -- "moon" (dark), "dawn" (light), or "rose" (default dark)
+        highlight_groups = {
+          Visual = { blend = 40 }, -- bright yellow background
+        }
+
       })
       vim.cmd("colorscheme rose-pine")
     end,
