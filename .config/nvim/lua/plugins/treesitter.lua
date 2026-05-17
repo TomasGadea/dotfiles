dde55b4f2214e36caf100e37e2890ff03bcbe8d0
TomasGadea <01tomas.gadea@gmail.com>
2026-05-18 01:19:27 +0200

    [nvim] Fix treesitter config for new API and update plugin lock
    - Replace removed nvim-treesitter.configs module with nvim-treesitter
    - Remove dead packer config comment block
    - Update lazy-lock.json with current plugin versions

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,9 @@
--- require'nvim-treesitter.configs'.setup {
---   ensure_installed = { "javascript", "typescript", "python", "sql", "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "dockerfile"},
---   sync_install = false,
---   auto_install = true,
---   highlight = {
---     enable = true,
---     additional_vim_regex_highlighting = false,
---   },
--- }
-
-
-
-
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   event = { 'BufReadPost', 'BufNewFile' },
   config = function()
-    require('nvim-treesitter.configs').setup {
+    require('nvim-treesitter').setup {
       ensure_installed = {
         "javascript", "typescript", "python", "sql",
         "c", "cpp", "lua", "vim", "vimdoc",
@@ -24,10 +11,6 @@ return {
       },
       sync_install = false,
       auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
     }
   end,
 }
